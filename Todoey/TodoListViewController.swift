7414f29e4eaf6e7ee58add7f152af8519c2cd36f
damdaris <darisadam@upi.edu>
2024-10-14 21:38:37 +0700

    add table view with checklist, and gitignore file

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Adam Daris Ryadhi on 14/10/24.
+//
+
+import UIKit
+
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Apples", "Bananas", "Oranges"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegates Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
+
